e8fc999a30aa8a264d3e90b3dde8acc96a627a6f
Gabe L'Herault <glherault@tcmsolutions.net>
2021-11-11 19:50:03 -0600

    Cleaned up code and made sure functions had access to  their required variables and reduced the variables in global

--- Assets/Logic/script.js
+++ Assets/Logic/script.js
@@ -1,83 +1,53 @@
-// Assignment Code
+// Assigned variable and event listener to generate button
+// Declared variable passwordLength in global because it's used in multiple functions.
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-let lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
-let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-let numBers = "1234567890".split("");
-let specialChar = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~".split("");
-
-let selectedArrays = [];
-
-
-let upBox = document.getElementById("Uppercase");
-let lwBox = document.getElementById("Lowercase");
-let nmBox = document.getElementById("Numbers");
-let spBox = document.getElementById("Specialchars");
+generateBtn.addEventListener("click", writePassword);
 let passwordLength = document.getElementById("PasswordLength");
 
-concatArrays();
-
-function concatArrays() {  
-  if (upBox.checked && lwBox.checked && nmBox.checked && spBox.checked === false) {
-        alert("Please select at least one character type to generate your password");
-        return;
-} if (lwBox.checked === true) {
-     selectedArrays = selectedArrays.concat(lowerCase);
-} if (upBox.checked === true) {
-     selectedArrays = selectedArrays.concat(upperCase);
-} if (nmBox.checked === true) {
-     selectedArrays = selectedArrays.concat(numBers);
-} if (spBox.checked === true) {
-     selectedArrays = selectedArrays.concat(specialChar);
-  } 
+// Primary function, declaring variables for the four character groups 
+// and created the strings required as well as linking their associated checkbox's to the document.
+// 
+function concatArrays() {
+  let lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+  let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  let numBers = "1234567890".split("");
+  let specialChar = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~".split("");
+  let upBox = document.getElementById("Uppercase");
+  let lwBox = document.getElementById("Lowercase");
+  let nmBox = document.getElementById("Numbers");
+  let spBox = document.getElementById("Specialchars");
+  let selectedArrays = [];
+if ((upBox.checked || lwBox.checked || nmBox.checked || spBox.checked) === false) {
+    alert("Please select at least one character type to generate your password");
+    return;
+  } if (lwBox.checked === true) {
+    selectedArrays = selectedArrays.concat(lowerCase);
+  } if (upBox.checked === true) {
+    selectedArrays = selectedArrays.concat(upperCase);
+  } if (nmBox.checked === true) {
+    selectedArrays = selectedArrays.concat(numBers);
+  } if (spBox.checked === true) {
+    selectedArrays = selectedArrays.concat(specialChar);
+  }
   return selectedArrays;
 }
 
-function generatePassword() {
+function generatePassword(selectedArrays) {
+  
   randomArray = [];
   for (let i = 0; i < passwordLength.valueAsNumber; i++) {
-      let randomNumber = Math.floor(Math.random()*selectedArrays.length);
-      randomArray = randomArray.concat(selectedArrays[randomNumber]);
+    let randomNumber = Math.floor(Math.random() * selectedArrays.length);
+    randomArray = randomArray.concat(selectedArrays[randomNumber]);
   }
   return randomArray.join('');
 }
 
-generateBtn.addEventListener("click", writePassword);
+function writePassword() {
 
-// if (selectedArrays === ) {
-  // }
-
-
+  var password = generatePassword(concatArrays());
+  var passwordText = document.querySelector("#password");
 
 
-// Add event listener to generate button
+  passwordText.value = password;
+}
 
-
-
-  // Get the checkbox
-
-
-// I did this first, even though Thomas said it was making more work.
-// if (upBox.checked === true) {
-//     upperAlpha = true;
-//   if (lwBox.checked === true)
-//     lowerAlpha = true;
-//   if (nmBox.checked === true)
-//     numBer = true;
-//   if (spBox.checked === true)
-//     specChar = true;
-// let lowerAlpha = [];
-// let upperAlpha = [];
-// let numBer = [];
-// let specChar = [];
-//     return 
-//   }  
-// }
