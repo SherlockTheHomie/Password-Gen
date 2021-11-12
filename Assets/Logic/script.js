3a30ddb672a729d71a7155a68ac0a89205ffb5af
Gabe L'Herault <glherault@tcmsolutions.net>
2021-11-11 20:13:25 -0600

    Finished adding user limits on password length and marking up comments in code

--- Assets/Logic/script.js
+++ Assets/Logic/script.js
@@ -4,9 +4,10 @@ var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 let passwordLength = document.getElementById("PasswordLength");
 
-// Primary function, declaring variables for the four character groups 
-// and created the strings required as well as linking their associated checkbox's to the document.
-// 
+// Concat arrays is the user faced function. This is assigning variables to the four character groups 
+// and created the arrays required for each group as well as linking their associated checkbox's to the document 
+// taking the users selected parameters and creating an array based off of it.
+
 function concatArrays() {
   let lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
   let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
@@ -19,7 +20,13 @@ function concatArrays() {
   let selectedArrays = [];
 if ((upBox.checked || lwBox.checked || nmBox.checked || spBox.checked) === false) {
     alert("Please select at least one character type to generate your password");
-    return;
+    return; // Created alert to remind user to select at least one character type to generate password
+  }  if (passwordLength.valueAsNumber < 8 || passwordLength.valueAsNumber > 128) { 
+    alert("Please enter a password length between 8 and 128");
+    return; // Created alert to remind user that the password length needs to be between 8 and 128
+  }  if (passwordLength.value === "") { 
+    alert("Please enter a password length");
+    return; // Created alert to remind user if the password length field was left empty before attempting to generate password 
   } if (lwBox.checked === true) {
     selectedArrays = selectedArrays.concat(lowerCase);
   } if (upBox.checked === true) {
@@ -29,9 +36,12 @@ if ((upBox.checked || lwBox.checked || nmBox.checked || spBox.checked) === false
   } if (spBox.checked === true) {
     selectedArrays = selectedArrays.concat(specialChar);
   }
-  return selectedArrays;
+  return selectedArrays; // Created a "checklist" pun intended, of if statements checking the boolean state of the check boxes so that the application knows which arrays the user has selected
 }
 
+// Password generation function, taking the array created by the user, assigning a number to each string in the array
+// and then selecting a random number from the string until the password length set by the user has been reached. Then
+// it converts the random numbers back into the characters from the array from which they were assigned.
 function generatePassword(selectedArrays) {
   
   randomArray = [];
@@ -41,7 +51,7 @@ function generatePassword(selectedArrays) {
   }
   return randomArray.join('');
 }
-
+// Primary function that calls the concatArrays function and prints the return from the generatePassword into the text area.
 function writePassword() {
 
   var password = generatePassword(concatArrays());
